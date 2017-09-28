b110808121aa6579f908e66306ed37b635c5ce13
Zygimantas <benetis.zygimantas@gmail.com>
2017-09-29 01:54:38 +0300

    feat(decision-tree) information gain ratio is counted correctly

--- machine-learning/decision-tree/src/main/scala/ID3.scala
+++ machine-learning/decision-tree/src/main/scala/ID3.scala
@@ -1,11 +1,10 @@
-
 object ID3 extends App {
 
   case class Node(value: Option[String])
 
   type TrainingInstance = Vector[String]
 
-  val names = Vector("x1", "x2", "x3", "Class")
+  val names = Vector("x1", "x2", "x3", "x4", "Class")
 
   val inputValues = Vector(
     //x1   x2   x3   x4   Cl
@@ -21,26 +20,60 @@ object ID3 extends App {
     Vector("b", "c", "c", "b", "G")
   )
 
-
   def learning() = {
     val rootNode = Node(None)
     divide_and_conquer(inputValues, rootNode)
   }
 
   def divide_and_conquer(
-    subset: Vector[TrainingInstance],
-    node: Node
+      subset: Vector[TrainingInstance],
+      node: Node
   ) = {
 
-    def hasOnlyOneClass(set: Vector[TrainingInstance]): Boolean = {
-      val amountOfClasses = set.map(_.last).size
-      val amountOfUniqueClasses = set.map(_.last).toSet.size
+    def hasOnlyOneClass(subset: Vector[TrainingInstance]): Boolean = {
+      val amountOfClasses = subset.map(_.last).size
+      val amountOfUniqueClasses = subset.map(_.last).toSet.size
 
       amountOfClasses == amountOfUniqueClasses
     }
 
-    if(hasOnlyOneClass(subset)) {
+    if (hasOnlyOneClass(subset)) {
       subset.head.last //Return Class
     }
   }
+
+  /**
+    * Split into subsets for specific param
+    * Find how many different classes in subset
+    */
+  def informationGain(subset: Vector[TrainingInstance],
+                      nthParam: Int): Double = {
+
+    type Param = String
+    val splitByParam: Map[Param, Vector[TrainingInstance]] =
+      subset.groupBy(identity((x: TrainingInstance) => x(nthParam)))
+
+    val log2 = (x: Double) => scala.math.log10(x) / scala.math.log10(2.0)
+
+    val setEntropy = splitByParam.map {
+      case (param, set: Vector[TrainingInstance]) =>
+        val classesCount = set.map(_.last).groupBy(identity)
+
+        val wholeSet: Double = subset.size
+
+        val thisSetSize: Double = classesCount.map(_._2.size).sum
+
+        val probabilityThisSetTaken = thisSetSize / wholeSet
+
+        val entropy = classesCount.foldLeft(0.0d) {
+          case (res: Double, (_, iSet: Vector[String])) =>
+            res + log2(iSet.size / thisSetSize) * (iSet.size / thisSetSize)
+        } * -1
+
+        probabilityThisSetTaken * entropy
+    }
+
+    setEntropy.sum
+  }
+
 }
--- machine-learning/decision-tree/src/test/scala/ID3Test.scala
+++ machine-learning/decision-tree/src/test/scala/ID3Test.scala
@@ -5,7 +5,69 @@ class ID3Test extends FreeSpec {
   "Divide and conquer" - {
     "return same class as element if passed one trainingSet" in {
 
-      assert(ID3.divide_and_conquer(Vector(Vector("P", "K")), Node(None)) === "K")
+      assert(ID3.divide_and_conquer(Vector(Vector("param1", "class")),
+        Node(None)) === "class")
     }
+  }
+
+  "Information gain" - {
+
+//    "entropy of two different elements should not be 0" in {
+//      assert(ID3.entropyOfParam(
+//        Vector("x1", "x2", "class"),
+//        Vector(
+//          Vector("1", "2", "A"),
+//          Vector("2", "4", "B"),
+//          Vector("1", "4", "B")
+//        )
+//      ) !== 0)
+//    }
+
+//    "two different elements entropy should be 1" in {
+//      assert(
+//        ID3.averageEntropy(
+//          Vector(
+//            Vector("1", "2", "A"),
+//            Vector("1", "2", "B")
+//          ), 2) === 1.0)
+//    }
+
+//    "entropy of two same elements should be 0" in {
+//      assert(
+//        ID3.averageEntropy(
+//          Vector(Vector("A", "A")), 1
+//        ) === 0.0)
+//    }
+
+    "should count information gain to be 0.5 as in example for hair" in {
+      assert(
+        ID3.informationGain(Vector(
+          Vector("g", "+"),
+          Vector("g", "-"),
+          Vector("r", "+"),
+          Vector("j", "-"),
+          Vector("j", "-"),
+          Vector("g", "+"),
+          Vector("j", "-"),
+          Vector("g", "-")
+        ),
+          0) === 0.5)
+    }
+
+    "should count information gain to be 0.95 as in example for size" in {
+      assert(
+        math.round(
+          ID3.informationGain(Vector(
+            Vector("m", "+"),
+            Vector("d", "-"),
+            Vector("d", "+"),
+            Vector("m", "-"),
+            Vector("d", "-"),
+            Vector("d", "+"),
+            Vector("d", "-"),
+            Vector("m", "-")
+          ),0) * 100.0)/100.0 === 0.95)
+    }
+
   }
 }
