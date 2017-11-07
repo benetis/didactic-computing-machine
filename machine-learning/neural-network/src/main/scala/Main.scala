8c73470dfbb77d303186214f76cbae0eca49e6b7
Zygimantas <benetis.zygimantas@gmail.com>
2017-11-07 13:42:31 +0200

    feat(neuron-networks) multiple outputs

--- machine-learning/neural-network/src/main/scala/Main.scala
+++ machine-learning/neural-network/src/main/scala/Main.scala
@@ -2,24 +2,23 @@
 
 object Main extends App {
   //
-  val nOutput      = 2
+  val nOutput      = 4
   val nInput       = 3
   val hiddenLayers = 1
   val learningRate = 1
-  val iterations = 20
+  val iterations = 200
   val myNN         = NN.initializeNN(nInput, nOutput, 1)
   val finishedNN = NN.trainNetwork(
     myNN,
     Vector(
       Vector(-3, -1, -3, 1),
-      Vector(-2, -1, -3, 1),
-      Vector(2, 0, 0, 0),
-      Vector(1, -1, 3, 1)
-//      Vector(-2, -3, -1, 1),
-//      Vector(2, -3, 0, 3),
-//      Vector(1, 1, -4, 1),
-//      Vector(3, -2, 0, 4),
-//      Vector(-1, -1, 1, 3)
+      Vector(2, 0, 0, 2),
+      Vector(1, -1, 3, 1),
+      Vector(-2, -3, -1, 1),
+      Vector(2, -3, 0, 3),
+      Vector(1, 1, -4, 1),
+      Vector(3, -2, 0, 4),
+      Vector(-1, -1, 1, 3)
     ),
     iterations,
     nOutput,
@@ -164,7 +163,7 @@ object NN {
         val (forward, outputs) = propogateForward(prev, curr._1)
 
         val expectedEmpty: Vector[Int] = Vector.fill(nOutput)(0)
-        val expected                   = expectedEmpty.updated(curr._1.last.toInt, 1)
+        val expected                   = expectedEmpty.updated(curr._1.last.toInt -1, 1)
 
         sumError += expected.zipWithIndex.foldLeft(0.0)((prev, curr) => {
           prev + Math.pow(curr._1 - outputs(curr._2), 2)
