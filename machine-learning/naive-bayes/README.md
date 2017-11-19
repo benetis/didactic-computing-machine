f8708cf1fb01d0ac3cccceaf32f5ac5c1a47d737
Zygimantas <benetis.zygimantas@gmail.com>
2017-11-19 15:04:40 +0200

    feat(naive-bayes) add data + data loader/parser

---
+++ machine-learning/naive-bayes/README.md
@@ -0,0 +1,3 @@
+# Naive Bayes spam filter
+
+Training data from https://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
--- machine-learning/naive-bayes/src/main/scala/example/NaiveBayes.scala
+++ machine-learning/naive-bayes/src/main/scala/example/NaiveBayes.scala
@@ -4,11 +4,14 @@ import breeze.linalg.DenseMatrix
 import scalaz.Scalaz._
 
 object SpamFilter extends App {
-  val input = Vector(
-    TrainInst(Vector("test", "testas"), "test"),
-    TrainInst(Vector("00", "000"), "00"),
-    TrainInst(Vector("00", "0000"), "00")
-  )
+//  val input = Vector(
+//    TrainInst(Vector("test", "testas"), "test"),
+//    TrainInst(Vector("00", "000"), "00"),
+//    TrainInst(Vector("00", "0000"), "00")
+//  )
+//
+
+  Loader.loadData()
 }
 
 case class TrainInst(words: Vector[String], category: String)
@@ -24,14 +27,16 @@ object WordFrequency {
   type WordFreq = Map[String, Int]
   type Category = String
 
-  def splitCategoriesWithFrequencies(trainingSet: Vector[TrainInst]): Map[Category, WordFreq] = {
+  def splitCategoriesWithFrequencies(
+      trainingSet: Vector[TrainInst]): Map[Category, WordFreq] = {
     val categoryMaps = splitIntoCategoryMaps(trainingSet)
     categoryMaps.mapValues(wordFrequencyList)
   }
 
   def wordFrequencyList(wordsList: Vector[String]): WordFreq = {
     wordsList
-      .groupBy(identity).mapValues(_.size)
+      .groupBy(identity)
+      .mapValues(_.size)
       .foldLeft(Map.empty[String, Int])((prev: WordFreq, curr) => {
         val word = curr._1
         val freq = curr._2
@@ -40,9 +45,44 @@ object WordFrequency {
   }
 
   def splitIntoCategoryMaps(
-    trainingSet: Vector[TrainInst]): Map[String, Vector[String]] = {
+      trainingSet: Vector[TrainInst]): Map[String, Vector[String]] = {
     trainingSet.foldLeft(Map.empty[String, Vector[String]])(
-      (prev, curr: TrainInst) =>
-        prev |+| Map(curr.category -> curr.words))
+      (prev, curr: TrainInst) => prev |+| Map(curr.category -> curr.words))
+  }
+}
+
+object Loader extends DataParser {
+
+  def loadData(): Vector[TrainInst] = {
+    val bufferedSource = io.Source.fromResource("./data.csv")
+    val data           = bufferedSource.getLines.drop(1).map(trimLine).toVector
+    bufferedSource.close
+    data
+  }
+}
+
+trait DataParser {
+  val spam    = "spam"
+  val notSpam = "ham"
+
+  val cleanSet = ",.!".toSet
+
+  def trimLine(line: String): TrainInst = {
+
+    def buildInstance(cat: String): TrainInst = {
+      TrainInst(line
+                  .drop(cat.length + 1)
+                  .split(" ")
+                  .map(_.trim)
+                  .map(_.filterNot(cleanSet))
+                  .toVector,
+                notSpam)
+    }
+
+    if (line.charAt(0) == spam.charAt(0)) {
+      buildInstance(spam)
+    } else {
+      buildInstance(notSpam)
+    }
   }
 }
