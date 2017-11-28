acc5dfb231bebc6f252348cbb8f262d96622f2a3
Zygimantas <benetis.zygimantas@gmail.com>
2017-11-28 20:40:20 +0200

    feat(naive-bayes) update to test performance of bayes

--- machine-learning/naive-bayes/src/main/scala/example/NaiveBayes.scala
+++ machine-learning/naive-bayes/src/main/scala/example/NaiveBayes.scala
@@ -6,27 +6,41 @@ import scalaz.Semigroup
 object SpamFilter extends App with DataParser {
   val data = Loader.loadData()
 
-  val testSentences: Vector[(String, String)] = Vector(
-    ("rate rate rate", "spam"),
-    ("i am going to sleep", "ham"),
-    ("urgent discount", "spam"),
-    ("i will call you later", "ham"),
-    ("call our customer service", "spam"),
-    ("free monthly ringtone", "spam"),
-    ("call freephone", "spam"),
-    ("wish you guys great semester", "ham"),
-    ("win free vip ticket", "spam")
-  )
+  val test = Loader.loadTest()
 
-  testSentences.foreach((curr: (String, String)) => {
+//  val testSentences: Vector[(String, String)] = Vector(
+//    ("rate rate rate", "spam"),
+//    ("i am going to sleep", "ham"),
+//    ("urgent discount", "spam"),
+//    ("i will call you later", "ham"),
+//    ("call our customer service", "spam"),
+//    ("free monthly ringtone", "spam"),
+//    ("call freephone", "spam"),
+//    ("wish you guys great semester", "ham"),
+//    ("win free vip ticket", "spam")
+//  )
+
+  var accurate       = 0
+  var countedAlready = 0
+
+  test.foreach((curr: TrainInst) => {
     val actual = NaiveBayes.classify(
-      curr._1.split(" ").toVector,
+      curr.words,
       WordFrequency.wordFrequencyList(data),
       data
     )
 
-    println(s"actual: $actual, expected: ${curr._2}")
+    if (curr.category == actual) {
+      accurate += 1
+      countedAlready += 1
+    } else {
+      countedAlready += 1
+    }
+
+    println(accurate.toDouble / countedAlready)
+
   })
+
 }
 
 case class TrainInst(words: Vector[String], category: String)
@@ -82,7 +96,7 @@ object NaiveBayes {
     val isCatA = catA - catB
 
     val lossIfGoodMarkedAsSpam = 1
-    val lossIfSpamMarkedAsGood = 2
+    val lossIfSpamMarkedAsGood = 1
     val isCatB =
       math.log((lossIfSpamMarkedAsGood * pB) / (lossIfGoodMarkedAsSpam * pA))
 
@@ -120,6 +134,13 @@ object Loader extends DataParser {
     bufferedSource.close
     data
   }
+
+  def loadTest(): Vector[TrainInst] = {
+    val bufferedSource = io.Source.fromResource("./test.csv")
+    val data           = bufferedSource.getLines.map(trimLine).toVector
+    bufferedSource.close
+    data
+  }
 }
 
 trait DataParser {
@@ -128,7 +149,182 @@ trait DataParser {
 
   val cleanSet = ",.!?:\"#+".toSet
 
-  val wordsToIgnore = Vector("and", "of", "the").toSet
+  val wordsToIgnore = Vector(
+    "a",
+    "about",
+    "above",
+    "after",
+    "again",
+    "against",
+    "all",
+    "am",
+    "an",
+    "and",
+    "any",
+    "are",
+    "aren't",
+    "as",
+    "at",
+    "be",
+    "because",
+    "been",
+    "before",
+    "being",
+    "below",
+    "between",
+    "both",
+    "but",
+    "by",
+    "can't",
+    "cannot",
+    "could",
+    "couldn't",
+    "did",
+    "didn't",
+    "do",
+    "does",
+    "doesn't",
+    "doing",
+    "don't",
+    "down",
+    "during",
+    "each",
+    "few",
+    "for",
+    "from",
+    "further",
+    "had",
+    "hadn't",
+    "has",
+    "hasn't",
+    "have",
+    "haven't",
+    "having",
+    "he",
+    "he'd",
+    "he'll",
+    "he's",
+    "her",
+    "here",
+    "here's",
+    "hers",
+    "herself",
+    "him",
+    "himself",
+    "his",
+    "how",
+    "how's",
+    "i",
+    "i'd",
+    "i'll",
+    "i'm",
+    "i've",
+    "if",
+    "in",
+    "into",
+    "is",
+    "isn't",
+    "it",
+    "it's",
+    "its",
+    "itself",
+    "let's",
+    "me",
+    "more",
+    "most",
+    "mustn't",
+    "my",
+    "myself",
+    "no",
+    "nor",
+    "not",
+    "of",
+    "off",
+    "on",
+    "once",
+    "only",
+    "or",
+    "other",
+    "ought",
+    "our",
+    "ours",
+    "ourselves",
+    "out",
+    "over",
+    "own",
+    "same",
+    "shan't",
+    "she",
+    "she'd",
+    "she'll",
+    "she's",
+    "should",
+    "shouldn't",
+    "so",
+    "some",
+    "such",
+    "than",
+    "that",
+    "that's",
+    "the",
+    "their",
+    "theirs",
+    "them",
+    "themselves",
+    "then",
+    "there",
+    "there's",
+    "these",
+    "they",
+    "they'd",
+    "they'll",
+    "they're",
+    "they've",
+    "this",
+    "those",
+    "through",
+    "to",
+    "too",
+    "under",
+    "until",
+    "up",
+    "very",
+    "was",
+    "wasn't",
+    "we",
+    "we'd",
+    "we'll",
+    "we're",
+    "we've",
+    "were",
+    "weren't",
+    "what",
+    "what's",
+    "when",
+    "when's",
+    "where",
+    "where's",
+    "which",
+    "while",
+    "who",
+    "who's",
+    "whom",
+    "why",
+    "why's",
+    "with",
+    "won't",
+    "would",
+    "wouldn't",
+    "you",
+    "you'd",
+    "you'll",
+    "you're",
+    "you've",
+    "your",
+    "yours",
+    "yourself",
+    "yourselves"
+  ).toSet
 
   def trimLine(line: String): TrainInst = {
 
