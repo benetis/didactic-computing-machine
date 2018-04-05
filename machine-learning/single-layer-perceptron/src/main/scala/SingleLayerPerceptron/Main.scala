646f0e01f4f99452a9b368862f3d21c562a5cf40
Zygimantas <benetis.zygimantas@gmail.com>
2018-04-05 13:57:11 +0300

    feat(project) predict function to calculate activate given weights

--- machine-learning/single-layer-perceptron/src/main/scala/SingleLayerPerceptron/Main.scala
+++ machine-learning/single-layer-perceptron/src/main/scala/SingleLayerPerceptron/Main.scala
@@ -1,11 +1,21 @@
+//Some code from https://machinelearningmastery.com/implement-perceptron-algorithm-scratch-python/
+//Some from Neural networks course by Ausra Saudargiene
+
 package SingleLayerPerceptron
 
 object Main extends App {
-  def main() = {
-
-  }
+  def main() = {}
 }
 
 object SingleLayerPerceptron {
+  def predict(row: Vector[Double], weights: Vector[Double]): Int = {
+    /* bias + w0 + w1 * x1 + w2 * x2 */
+    val activation = weights.head + row
+      .drop(1)
+      .zipWithIndex
+      .foldLeft(0.0)((prev, curr: (_, Int)) =>
+        prev + weights(curr._2 + 1) * row(curr._2))
 
+    if(activation >= 0.0) 1 else 0
+  }
 }
--- machine-learning/single-layer-perceptron/src/test/scala/SingleLayerPerceptron/Test.scala
+++ machine-learning/single-layer-perceptron/src/test/scala/SingleLayerPerceptron/Test.scala
@@ -3,7 +3,26 @@ package SingleLayerPerceptron
 import org.scalatest._
 
 class Test extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+
+
+  /* Two inputs + bias */
+  val weights = Vector(-0.2, 0.4, 0.4)
+
+  "Predict" should "return class 0 with this row and weights" in {
+
+    val row1 = Vector(0.0, 0.1, 0)
+    val row2 = Vector(0.2, 0.1, 0)
+
+    SingleLayerPerceptron.predict(row1, weights) shouldEqual 0
+    SingleLayerPerceptron.predict(row2, weights) shouldEqual 0
+  }
+
+
+  "Predict" should "return class 1 with this row and weights" in {
+    val row3 = Vector(0.9, 0.9, 1)
+    val row4 = Vector(0.7, 0.8, 1)
+
+    SingleLayerPerceptron.predict(row3, weights) shouldEqual 1
+    SingleLayerPerceptron.predict(row4, weights) shouldEqual 1
   }
 }
