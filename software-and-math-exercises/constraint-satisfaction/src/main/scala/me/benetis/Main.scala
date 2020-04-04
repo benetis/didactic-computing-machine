50d65a21fc6ba6b12f7bfb1ce2fb21e7c91ef4b5
Zygimantas <zygimantas@benetis.me>
2020-04-04 12:40:13 +0300

    feat(csp) working 8 queens

--- software-and-math-exercises/constraint-satisfaction/src/main/scala/me/benetis/Main.scala
+++ software-and-math-exercises/constraint-satisfaction/src/main/scala/me/benetis/Main.scala
@@ -1,35 +1,44 @@
 package me.benetis
 
-object Main extends  App {
+object Main extends App {
 
   case class Square(x: Int, y: Int)
 
   type PlacedQueens = Vector[Square]
 
-
   def isChecked(p1: Square, p2: Square): Boolean = {
     p1.x == p2.x ||
     p1.y == p2.y ||
-      Math.abs(p1.x - p2.x)== Math.abs(p1.y - p2.y)
+    Math.abs(p1.x - p2.x) == Math.abs(p1.y - p2.y)
   }
 
   def wholeBoard: Vector[Square] = {
-    Range.inclusive(1, 8).flatMap(x => {
-      Range.inclusive(1, 8).map(y => {
-        Square(x, y)
+    Range
+      .inclusive(1, 8)
+      .flatMap(x => {
+        Range
+          .inclusive(1, 8)
+          .map(y => {
+            Square(x, y)
+          })
       })
-    }).toVector
+      .toVector
   }
 
   def queensCheckedSquares(queen: Square): Vector[Square] = {
     wholeBoard.filter(sq => isChecked(queen, sq))
   }
 
-  def canBePlaced(queenToPlace: Square, availableSquares: Vector[Square]): Option[Vector[Square]] = {
+  def canBePlaced(queenToPlace: Square,
+                  availableSquares: Vector[Square]): Option[Vector[Square]] = {
 
-    if(availableSquares.nonEmpty) {
+    if (availableSquares.nonEmpty) {
       if (availableSquares.contains(queenToPlace))
-        Some(availableSquares.toSet.diff(queensCheckedSquares(queenToPlace).toSet).toVector)
+        Some(
+          availableSquares.toSet
+            .diff(queensCheckedSquares(queenToPlace).toSet)
+            .toVector
+        )
       else
         None
     } else {
@@ -37,27 +46,30 @@ object Main extends  App {
     }
   }
 
-  def placeQueens(toPlace: Int, available: Vector[Square]): Option[PlacedQueens] = {
+  def placeQueens(toPlace: Int,
+                  available: Vector[Square]): Option[PlacedQueens] = {
 
-    if(toPlace <= 0) {
+    if (toPlace <= 0) {
       Some(Vector.empty[Square])
     } else {
-      val result = available.foldLeft(Vector.empty[Square])((prev, queenToPlace: Square) => {
-        val afterQueen = canBePlaced(queenToPlace, available)
+      val result = available.foldLeft(Vector.empty[Square])(
+        (prev, queenToPlace: Square) => {
+          val afterQueen = canBePlaced(queenToPlace, available)
 
-        afterQueen match {
-          case Some(nextSquares) =>
-            placeQueens(toPlace - 1, nextSquares) match {
-              case Some(value) =>
-                queenToPlace +: value
-              case None => prev
-            }
-          case None => prev
+          afterQueen match {
+            case Some(nextSquares) =>
+              placeQueens(toPlace - 1, nextSquares) match {
+                case Some(value) =>
+                  queenToPlace +: value
+                case None => prev
+              }
+            case None => prev
+          }
+
         }
+      )
 
-      })
-
-      if(result.isEmpty)
+      if (result.isEmpty)
         None
       else
         Some(result)
@@ -68,19 +80,23 @@ object Main extends  App {
 
     println(placedQueens)
 
-    Range.inclusive(1, 8).foreach(x => {
-      Range.inclusive(1, 8).foreach(y => {
-        if(placedQueens.contains(Square(x, y)))
-          print(s"X ")
-        else
-          print(s"- ")
+    Range
+      .inclusive(1, 8)
+      .foreach(x => {
+        Range
+          .inclusive(1, 8)
+          .foreach(y => {
+            if (placedQueens.contains(Square(x, y)))
+              print(s"X ")
+            else
+              print(s"- ")
+          })
+        println()
       })
-      println()
-    })
   }
 
   placeQueens(8, wholeBoard) match {
-    case Some(value) =>   printQueens(value)
-    case None => println("no solutions are possible")
+    case Some(value) => printQueens(value)
+    case None        => println("no solutions are possible")
   }
 }
