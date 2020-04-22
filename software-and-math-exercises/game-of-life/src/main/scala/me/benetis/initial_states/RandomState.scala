456cb55587c47ed876fd28e120c43f527c7b00d6
Zygimantas <zygimantas@benetis.me>
2020-04-22 21:53:20 +0300

    feat(game-of-life) cleanup + middle of screen blob

--- software-and-math-exercises/game-of-life/src/main/scala/me/benetis/initial_states/RandomState.scala
+++ software-and-math-exercises/game-of-life/src/main/scala/me/benetis/initial_states/RandomState.scala
@@ -1,19 +1,31 @@
 package me.benetis.initial_states
 
+import me.benetis.visual.Render.RenderConfig
 import me.benetis.{LifeState, Point}
 
 object RandomState {
 
-  def randomState(): LifeState = {
+  def randomCenter(renderConfig: RenderConfig): LifeState = {
 
-    val rows = Range.inclusive(10, 20)
-    val columns = Range.inclusive(10, 20)
+    val maxX: Int = (renderConfig.browserWidth / renderConfig.scale).round.toInt
+    val maxY: Int =
+      (renderConfig.browserHeight / renderConfig.scale).round.toInt
 
-    val evenR = rows.filter(_ % 2 == 0)
-    val evenC = rows.filter(_ % 2 == 0)
+    val columns = Range.inclusive(
+      (maxX / 2) - renderConfig.blobSize,
+      (maxX / 2) + renderConfig.blobSize
+    )
+    val rows = Range.inclusive(
+      (maxY / 2) - renderConfig.blobSize,
+      (maxY / 2) + renderConfig.blobSize
+    )
 
+    randomState(rows = rows, columns = columns)
+
+  }
+
+  private def randomState(rows: Range, columns: Range): LifeState = {
     rows.flatMap(i => columns.map(j => Point(i, j))).toSet
-
   }
 
 }
--- software-and-math-exercises/game-of-life/src/main/scala/me/benetis/visual/Render.scala
+++ software-and-math-exercises/game-of-life/src/main/scala/me/benetis/visual/Render.scala
@@ -8,74 +8,79 @@ import scala.util.Random
 import zio.clock.Clock
 import zio.{IO, Ref, Schedule, Task, UIO, ZIO}
 import zio.duration._
-object Render {
 
-  val scale = 20
+object Render {
+  case class RenderConfig(scale: Int,
+                          blobSize: Int,
+                          browserWidth: Double,
+                          browserHeight: Double)
+
+  val config =
+    RenderConfig(10, 20, dom.window.innerWidth, dom.window.innerHeight)
 
   val rng = new Random()
 
-  val canvas: html.Canvas =
-    document.querySelector("#canvas").asInstanceOf[html.Canvas]
-
-  val browserWidth = dom.window.innerWidth
-  val browserHeight = dom.window.innerHeight
-
-  canvas.width = browserWidth.toInt
-  canvas.height = browserHeight.toInt
-
   val program: ZIO[Clock, Throwable, Unit] = {
-
-    val renderer = canvas
-      .getContext("2d")
-      .asInstanceOf[dom.CanvasRenderingContext2D]
-
     for {
-      ref <- Ref.make(RandomState.randomState())
-      _ <- (render(renderer, ref) *> updateState(ref) *> UIO(
+      renderer <- prepareScreen(config)
+      refState <- Ref.make(RandomState.randomCenter(config))
+      _ <- (render(renderer, config, refState) *> updateState(refState) *> UIO(
         dom.console.log("tick")
       )).repeat(Schedule.fixed(1.second)).forever
     } yield ()
   }
 
-  def updateState(ref: Ref[LifeState]): ZIO[Any, Nothing, Unit] =
+  private def render(renderer: CanvasRenderingContext2D,
+                     config: RenderConfig,
+                     ref: Ref[LifeState]): ZIO[Any, Nothing, Unit] = {
+    for {
+      _ <- clearCanvas(renderer, config)
+      state <- ref.get
+      _ = state.foreach(p => renderPoint(renderer, config, p))
+    } yield ()
+  }
+
+  private def updateState(ref: Ref[LifeState]): ZIO[Any, Nothing, Unit] =
     for {
       _ <- ref.update(state => GameOfLifeRules.nextState(state))
     } yield ()
 
-  def render(renderer: CanvasRenderingContext2D,
-             ref: Ref[LifeState]): ZIO[Any, Nothing, Unit] = {
-
-//    val minX = state.minBy(_.x).x
-//    val maxX = state.maxBy(_.x).x
-//
-//    val minY = state.minBy(_.y).y
-//    val maxY = state.maxBy(_.y).y
-//
-//    val rows = Range.inclusive(minY, maxY)
-//    val columns = Range.inclusive(minX, maxX)
-//
-//    val emptyMatrix: Seq[IndexedSeq[Point]] =
-//      rows.map(r => columns.map(c => Point(c, r)))
-    for {
-      _ <- clearCanvas(renderer)
-      state <- ref.get
-      _ = state.foreach(p => renderPoint(renderer, p))
-    } yield ()
-  }
-
-  private def clearCanvas(renderer: CanvasRenderingContext2D): UIO[Unit] = UIO {
+  private def clearCanvas(renderer: CanvasRenderingContext2D,
+                          config: RenderConfig): UIO[Unit] = UIO {
     renderer.fillStyle = "black"
-    renderer.fillRect(0, 0, browserWidth, browserHeight)
+    renderer.fillRect(0, 0, config.browserWidth, config.browserHeight)
   }
 
   private def renderPoint(renderer: CanvasRenderingContext2D,
+                          config: RenderConfig,
                           point: Point): Unit = {
 
     val colors = Vector("red", "yellow")
 
     val randomColor: String = colors(rng.nextInt(colors.length))
     renderer.fillStyle = randomColor
-    renderer.fillRect(point.x * scale, point.y * scale, scale, scale)
+    renderer.fillRect(
+      point.x * config.scale,
+      point.y * config.scale,
+      config.scale,
+      config.scale
+    )
+  }
+
+  private def prepareScreen(
+    config: RenderConfig
+  ): UIO[CanvasRenderingContext2D] = UIO {
+    val canvas: html.Canvas =
+      document.querySelector("#canvas").asInstanceOf[html.Canvas]
+
+    canvas.width = config.browserWidth.toInt
+    canvas.height = config.browserHeight.toInt
+
+    val renderer = canvas
+      .getContext("2d")
+      .asInstanceOf[dom.CanvasRenderingContext2D]
+
+    renderer
   }
 
 }
