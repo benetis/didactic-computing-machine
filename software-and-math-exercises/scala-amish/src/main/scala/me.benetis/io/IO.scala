fd83d6824cff9e0e82feef264f9fdebfe0add73d
benetis <git@benetis.me>
2022-03-26 17:40:33 +0200

    feat(io) interpret with flatmap case class

--- software-and-math-exercises/scala-amish/src/main/scala/me.benetis/io/IO.scala
+++ software-and-math-exercises/scala-amish/src/main/scala/me.benetis/io/IO.scala
@@ -2,21 +2,15 @@ package me.benetis
 package io
 
 sealed trait IO[A] { self =>
-  def run: A
-  def map[B](f: A => B): IO[B] =
-    new IO[B] {
-      def run = f(self.run)
-    }
-  def flatMap[B](f: A => IO[B]): IO[B] =
-    new IO[B] {
-      def run: B = f(self.run).run
-    }
+  def flatMap[B](f: A => IO[B]): IO[B] = IO.FlatMap(self, f)
+  def map[B](f: A => B): IO[B] = flatMap(a => IO.pure(f(a)))
   def *>[B](right: IO[B]): IO[B] = flatMap(_ => right)
 }
 
 object IO {
-  case class Effect[A](a: () => A) extends IO[A] {
-    def run: A = a()
-  }
+  case class Pure[A](a: () => A) extends IO[A]
+  case class FlatMap[A, B](ctx: IO[A], f: A => IO[B]) extends IO[B]
+  case class Effect[A](a: () => A) extends IO[A] 
   def effect[A](a: => A): IO[A] = Effect(() => a)
+  def pure[A](a: A): IO[A] = Pure(() => a)
 }
