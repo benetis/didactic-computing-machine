a6c282ec39ae612820a2fe5d7f621a2a2873d853
benetis <git@benetis.me>
2023-06-03 14:32:08 +0300

    feat(euler, project) cleanup

--- software-and-math-exercises/ruby-euler/spec/ruby/euler2_spec.rb
+++ software-and-math-exercises/ruby-euler/spec/ruby/euler2_spec.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe "Euler2" do
-  before do
-    # Do nothing
-  end
-
-  after do
-    # Do nothing
-  end
-
   it "should calculate sum square difference" do
     expect(Euler2::SumSquareDifference.calculate).to eq(25164150)
   end
