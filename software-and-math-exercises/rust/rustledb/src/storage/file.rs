39644a592c33b2cfa53095fb7be71d72c7ef164d
benetis <git@benetis.me>
2023-09-30 13:32:11 +0300

    rustledb: change storage to use Vec instead of slices

--- software-and-math-exercises/rust/rustledb/src/storage/file.rs
+++ software-and-math-exercises/rust/rustledb/src/storage/file.rs
@@ -5,8 +5,8 @@ use crate::storage::page::PAGE_SIZE;
 pub trait StorageAlgebra {
     fn create(file_path: String) -> Result<Box<Self>, StorageError>;
     fn load(file_path: String) -> Result<Box<Self>, StorageError>;
-    fn read_block(&mut self, block_id: usize, buffer: &mut [u8]) -> Result<(), StorageError>;
-    fn write_block(&mut self, block_id: usize, buffer: &[u8]) -> Result<(), StorageError>;
+    fn read_block(&mut self, block_id: usize, buffer: &mut Vec<u8>) -> Result<(), StorageError>;
+    fn write_block(&mut self, block_id: usize, buffer: Vec<u8>) -> Result<(), StorageError>;
 }
 
 #[derive(Debug)]
@@ -42,7 +42,7 @@ impl StorageAlgebra for FileStorage {
         }))
     }
 
-    fn read_block(&mut self, page_id: usize, buffer: &mut [u8]) -> Result<(), StorageError> {
+    fn read_block(&mut self, page_id: usize, buffer: &mut Vec<u8>) -> Result<(), StorageError> {
         if buffer.len() != PAGE_SIZE {
             return Err(StorageError::Custom("Buffer size does not match page size".into()));
         }
@@ -50,10 +50,10 @@ impl StorageAlgebra for FileStorage {
         self.file_handle.seek(SeekFrom::Start((page_id * PAGE_SIZE) as u64))
             .map_err(|e| StorageError::File(e))?;
 
-        self.file_handle.read_exact(buffer).map_err(|e| StorageError::File(e))
+        self.file_handle.read_exact(&mut *buffer).map_err(|e| StorageError::File(e))
     }
 
-    fn write_block(&mut self, block_id: usize, buffer: &[u8]) -> Result<(), StorageError> {
+    fn write_block(&mut self, block_id: usize, buffer: Vec<u8>) -> Result<(), StorageError> {
         if buffer.len() != PAGE_SIZE {
             return Err(StorageError::Custom("Buffer size does not match page size".into()));
         }
@@ -63,7 +63,7 @@ impl StorageAlgebra for FileStorage {
             .map_err(|e| StorageError::File(e))?;
 
         self.file_handle
-            .write_all(buffer)
+            .write_all(&*buffer)
             .map_err(|e| StorageError::File(e))
     }
 }
--- software-and-math-exercises/rust/rustledb/src/storage/page_manager.rs
+++ software-and-math-exercises/rust/rustledb/src/storage/page_manager.rs
@@ -39,17 +39,17 @@ impl<Storage: StorageAlgebra> PageManager<Storage> {
 
         let new_page = ColumnarPage::new(new_id, columnar_type.get_length());
         let buffer = new_page.to_buffer();
-        self.storage.write_block(new_page.id, &buffer)?;
+        self.storage.write_block(new_page.id, buffer)?;
 
         Ok(new_page)
     }
 
     fn read_page(&mut self, page_id: usize) -> Result<ColumnarPage, StorageError> {
-        let mut buffer = [0; PAGE_SIZE];
+        let mut buffer = Vec::with_capacity(PAGE_SIZE);
 
         self.storage.read_block(page_id, &mut buffer)?;
 
-        let (header, data) = buffer.split_at(PAGE_HEADER_SIZE);
+        let (header, data) = &buffer.split_at(PAGE_HEADER_SIZE);
 
         Ok(ColumnarPage {
             id: page_id,
@@ -73,41 +73,45 @@ mod tests {
     use super::*;
     use crate::storage::columnar_type::ColumnarType;
     use std::collections::HashMap;
-    use std::convert::TryInto;
 
     struct MockStorage {
-        pages: HashMap<usize, [u8; PAGE_SIZE]>,
+        blocks: HashMap<usize, Vec<u8>>,
     }
 
     impl StorageAlgebra for MockStorage {
         fn create(_file_path: String) -> Result<Box<Self>, StorageError> {
             Ok(Box::new(MockStorage {
-                pages: HashMap::new(),
+                blocks: HashMap::new(),
             }))
         }
 
         fn load(_file_path: String) -> Result<Box<Self>, StorageError> {
             Ok(Box::new(MockStorage {
-                pages: HashMap::new(),
+                blocks: HashMap::new(),
             }))
         }
 
-        fn read_block(&mut self, page_id: usize, buffer: &mut [u8]) -> Result<(), StorageError> {
-            buffer.copy_from_slice(&self.pages[&page_id]);
-            Ok(())
+        fn read_block(&mut self, page_id: usize, buffer: &mut Vec<u8>) -> Result<(), StorageError> {
+            if let Some(block) = self.blocks.get(&page_id) {
+                buffer.resize(block.len(), 0);
+                buffer.copy_from_slice(block);
+                Ok(())
+            } else {
+                Err(StorageError::Custom("page doesnt exist".parse().unwrap()))
+            }
         }
 
-        fn write_block(&mut self, page_id: usize, buffer: &[u8]) -> Result<(), StorageError> {
-            self.pages.insert(page_id, buffer.try_into().unwrap());
+        fn write_block(&mut self, block_id: usize, buffer: Vec<u8>) -> Result<(), StorageError> {
+            self.blocks.insert(block_id, buffer.to_vec());
             Ok(())
         }
     }
 
     #[test]
     fn test_create_page_for_table() {
-        let mock_storage = MockStorage::create("".into()).unwrap();
+        let mock_storage = MockStorage::create("x".into()).unwrap();
         let mut page_manager = PageManager::new(mock_storage);
-
+        //
         let table_id = 1;
         let columnar_type = ColumnarType::Int(4);
         let page = page_manager.create_page_for_table(table_id, columnar_type).unwrap();
@@ -116,41 +120,35 @@ mod tests {
         assert_eq!(page_manager.get_table_of_page(page.id).unwrap(), table_id);
     }
 
-    // #[test]
-    // fn test_read_page() {
-    //     let mock_storage = MockStorage::create("".into()).unwrap();
-    //     let mut page_manager = PageManager::new(mock_storage);
-    //
-    //     let table_id = 1;
-    //     let columnar_type = ColumnarType::Int; // Assuming you have this enum variant
-    //     let page = page_manager.create_page_for_table(table_id, columnar_type).unwrap();
-    //
-    //     let read_page = page_manager.read_page(page.id).unwrap();
-    //
-    //     // Check if the read page matches the created one
-    //     assert_eq!(read_page.id, page.id);
-    //     assert_eq!(read_page.data, page.data);
-    //     assert_eq!(read_page.header.free_space_offset, page.header.free_space_offset);
-    //     // Add more assertions for other fields if necessary
-    // }
-    //
-    // #[test]
-    // fn test_generate_new_page_id() {
-    //     let mock_storage = MockStorage::create("".into()).unwrap();
-    //     let mut page_manager = PageManager::new(mock_storage);
-    //
-    //     let first_id = page_manager.generate_new_page_id();
-    //     assert_eq!(first_id, 1);
-    //
-    //     // Simulate deleting a page, which would push its ID to free_page_ids
-    //     page_manager.free_page_ids.push(first_id);
-    //
-    //     // The next generated ID should reuse the free ID
-    //     let second_id = page_manager.generate_new_page_id();
-    //     assert_eq!(second_id, first_id);
-    //
-    //     // Now, with no free IDs, it should generate a new one
-    //     let third_id = page_manager.generate_new_page_id();
-    //     assert_eq!(third_id, 2);
-    // }
+    #[test]
+    fn test_read_page() {
+        let mock_storage = MockStorage::create("x".into()).unwrap();
+        let mut page_manager = PageManager::new(mock_storage);
+
+        let table_id = 1;
+        let columnar_type = ColumnarType::Int(4);
+        let page = page_manager.create_page_for_table(table_id, columnar_type).unwrap();
+
+        let read_page = page_manager.read_page(page.id).unwrap();
+
+        assert_eq!(read_page.id, page.id);
+        assert_eq!(read_page.data, page.data);
+    }
+
+    #[test]
+    fn test_generate_new_page_id() {
+        let mock_storage = MockStorage::create("x".into()).unwrap();
+        let mut page_manager = PageManager::new(mock_storage);
+
+        let first_id = page_manager.generate_new_page_id();
+        assert_eq!(first_id, 1);
+
+        page_manager.free_page_ids.push(first_id);
+
+        let second_id = page_manager.generate_new_page_id();
+        assert_eq!(second_id, first_id);
+
+        let third_id = page_manager.generate_new_page_id();
+        assert_eq!(third_id, 2);
+    }
 }
