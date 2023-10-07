8136db8900e35d17571fa301182c0b755d2a3ffe
benetis <git@benetis.me>
2023-10-07 15:14:00 +0300

    mini-redis: sharded locks

--- software-and-math-exercises/rust/mini-redis/src/main.rs
+++ software-and-math-exercises/rust/mini-redis/src/main.rs
@@ -5,18 +5,19 @@ use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 use std::sync::Mutex;
 
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+// type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+    let db: ShardedDb = new_sharded_db(16);
 
     loop {
         let (socket, ip) = listener.accept().await.unwrap();
 
-        let db: Db = db.clone();
+        let db = db.clone();
 
         println!("Accepted connection from {:?}", ip);
 
@@ -26,20 +27,24 @@ async fn main() {
     }
 }
 
-async fn process(socket: TcpStream, ip: std::net::SocketAddr, db: Db) {
+async fn process(socket: TcpStream, ip: std::net::SocketAddr, db: ShardedDb) {
     use mini_redis::Command::{self, Get, Set};
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
+                let key = cmd.key().to_string();
+                let shard_index = hash(&key) % db.len();
+                let mut shard = db[shard_index].lock().unwrap();
+                shard.insert(key, cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
-            Get(_) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get("hello") {
+            Get(cmd) => {
+                let key = cmd.key().to_string();
+                let shard_index = hash(&key) % db.len();
+                let shard = db[shard_index].lock().unwrap();
+                if let Some(value) = shard.get(&key) {
                     Frame::Bulk(value.clone().into())
                 } else {
                     Frame::Null
@@ -51,4 +56,16 @@ async fn process(socket: TcpStream, ip: std::net::SocketAddr, db: Db) {
         connection.write_frame(&response).await.unwrap();
     }
 
+}
+
+fn hash(s: &str) -> usize {
+    s.bytes().fold(0, |acc, b| acc.wrapping_add(b as usize))
+}
+
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
 }
