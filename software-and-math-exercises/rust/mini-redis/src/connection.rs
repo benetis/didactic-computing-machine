7cd1fb1fc454ffa8fe2ef7ea360f82d9b5226459
benetis <git@benetis.me>
2023-10-08 07:53:58 +0300

    mini-redis: frames, read frame wip

---
+++ software-and-math-exercises/rust/mini-redis/src/connection.rs
@@ -0,0 +1,38 @@
+use bytes::BytesMut;
+use tokio::net::TcpStream;
+use mini_redis::{Frame, Result};
+use tokio::io::AsyncReadExt;
+
+struct Connection {
+    stream: TcpStream,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            buffer: BytesMut::with_capacity(4096)
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            if self.stream.read_buf(&mut self.buffer).await? == 0 {
+                return if self.buffer.is_empty() {
+                    Ok(None)
+                } else {
+                    Err("connection reset by peer".into())
+                }
+            }
+        }
+    }
+
+    fn parse_frame(&mut self) -> Result<Option<Frame>> {
+        todo!()
+    }
+}
---
+++ software-and-math-exercises/rust/mini-redis/src/main.rs
@@ -0,0 +1,3 @@
+mod connection;
+
+fn main() {}
