f6b6a473074984b18231f281b15cdf8c3a4f244e
benetis <git@benetis.me>
2023-11-04 13:39:34 +0200

    ruby: cleanup

--- software-and-math-exercises/ruby-aoc2022/lib/ruby/aoc2022/calorie_counting/input.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Input
-  class << self
-    def from_file(file)
-      File.read(file).split("\n")
-    end
-  end
-end
--- software-and-math-exercises/ruby-aoc2022/lib/ruby/aoc2022/calorie_counting/output.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Output
-  class << self
-    def to_console(output)
-      puts output
-    end
-  end
-end
