7ea19672cf1cb0593e6415cce8978efa04a6f307
benetis <git@benetis.me>
2023-11-25 14:52:15 +0200

    two_sum: o(N) instead of o(N^2) with storing complements in hashmaps

--- software-and-math-exercises/rust/practice/src/two_sum.rs
+++ software-and-math-exercises/rust/practice/src/two_sum.rs
@@ -1,27 +1,21 @@
+use std::collections::HashMap;
+
 pub struct TwoSum;
 
 impl TwoSum {
     pub fn calculate(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut i = 0;
-        let mut res = vec![];
+        let mut map = HashMap::new();
 
-        while i < nums.len() {
-            let start_with_next = i + 1;
+        for (i, &num) in nums.iter().enumerate() {
+            let diff = target - num;
 
-            for j in start_with_next..nums.len() {
-                if nums[i] + nums[j] == target {
-                    res = vec![i as i32, j as i32];
-                    break;
-                }
+            if let Some(&index) = map.get(&num) {
+                return vec![index as i32, i as i32];
             }
 
-            if !res.is_empty() {
-                break;
-            }
-
-            i += 1;
+            map.insert(diff, i);
         }
 
-        res
+        vec![]
     }
 }
