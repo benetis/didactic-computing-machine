633d1a50bdc6b33190f9c02081d33e577bab804e
benetis <git@benetis.me>
2023-12-02 21:22:32 +0200

    wasm: game of life, working universe interop

--- wasm/tutorial-game-of-life/www/index.js
+++ wasm/tutorial-game-of-life/www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from "tutorial-game-of-life";
+import {Universe} from "tutorial-game-of-life";
 
-wasm.greet();
+const pre = document
+    .getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
